08ea777bb8856a6461139a28ca9d30d6b6b65e5f
ThembelihleQueeneth <malukathembelihle95@gmail.com>
2025-11-07 09:54:53 +0200

    Recent Changes

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,13 +1,21 @@
 import { query } from "../config/database";
 import { IUser, NewRegister, UpdateRegister } from "../types/user";
+import bcrypt from 'bcryptjs';
 
-// Add new user
+
+//CRUD - Create, Read, Update, Delete
+
+//Create a User
+
 export const createUser = async (appData: NewRegister): Promise<IUser> => {
   const { name, email, password_hash, role, display_picture } = appData;
 
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password_hash, salt);
+
   const { rows } = await query(
     "INSERT INTO users (name, email, password_hash, role, display_picture) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-    [name, email, password_hash, role, display_picture]
+    [name, email, hashedPassword, role, display_picture]
   );
 
   return rows[0];
@@ -25,7 +33,8 @@ export const findUserById = async (id: number): Promise<IUser | null> => {
   return rows[0] || null;
 };
 
-// Update user role
+// Update user role by id
+
 export const updateUserRole = async (
   id: number,
   appData: UpdateRegister
@@ -38,7 +47,7 @@ export const updateUserRole = async (
   return rows[0] || null;
 };
 
-// Delete user
+// Delete user by id
 export const deleteUser = async (id: number): Promise<IUser | null> => {
   const { rows } = await query("DELETE FROM users WHERE id = $1 RETURNING *", [
     id,
